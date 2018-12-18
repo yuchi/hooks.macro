d725da57b40ef74fe4776f24027caf2236025beb
Pier Paolo Ramon <ramonpierre@gmail.com>
2018-12-18 01:29:04 +0100

    [Typings] Add Flow and TypeScript types for all the API surface

--- src/hooks.macro.d.ts
+++ src/hooks.macro.d.ts
@@ -2,3 +2,8 @@ export function useAutoMemo<T>(factory: () => T): T;
 export function useAutoMemo<T>(factory: T): T;
 
 export function useCallback<T extends (...args: any[]) => any>(callback: T): T;
+
+type EffectCallback = () => void | (() => void);
+
+export function useEffect(effect: EffectCallback): void;
+export function useLayoutEffect(effect: EffectCallback): void;
